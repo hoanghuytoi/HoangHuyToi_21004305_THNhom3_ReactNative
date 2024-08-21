7cc3f181b760785aa6d469ac6ffa8caed28b156c
hoanghuytoi <hoanghuytoi03@Gmail.com>
2024-08-21 10:23:09 +0700

    Tuan1_BaiTap2

--- Tuan01/Lab01.js
+++ Tuan01/Lab01.js
@@ -30,7 +30,6 @@ if(markBMI > johnBMI){
 
 // Coding Challenge #3
 //There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
-
 //1. Calculate the average score for each team, using the test data below
 dolphins = [96, 108, 89];
 koalas = [88, 91, 110];
@@ -50,6 +49,7 @@ if(averageDolphins > averageKoalas){
 }else{
     console.log("It's a draw!");
 }
+
 //3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a 
 //score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks
 if(averageDolphins > averageKoalas && averageDolphins >= 100){
@@ -60,11 +60,15 @@ if(averageDolphins > averageKoalas && averageDolphins >= 100){
     console.log("No team wins the trophy!");
 }
 
-
 //4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 
 // points. Otherwise, no team wins the trophy
-//Test data:
-//§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-//§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-//§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+if(averageDolphins > averageKoalas && averageDolphins >= 100){
+    console.log("Dolphins win the trophy!");
+}else if(averageDolphins < averageKoalas && averageKoalas >= 100){
+    console.log("Koalas win the trophy!");
+}else if(averageDolphins === averageKoalas && averageDolphins >= 100 && averageKoalas >= 100){
+    console.log("It's a draw!");
+}else{
+    console.log("No team wins the trophy!");
+}
 
